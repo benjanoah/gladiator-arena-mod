ad7a5cd774040d97ac150febed57f948f312fcf3
benjanoah <benjaminneuteboom@outlook.com>
2026-02-16 09:52:21 +0100

    Initial Gladiator Arena Mod - IT WORKS! ⚔️🏛️

---
+++ gladiator-arena-mod/src/main/java/com/benjanoah/gladiatorarena/GladiatorArenaMod.java
@@ -0,0 +1,15 @@
+package com.benjanoah.gladiatorarena;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GladiatorArenaMod implements ModInitializer {
+    public static final String MOD_ID = "gladiator-arena-mod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Gladiator Arena Mod is loading! ⚔️🏛️");
+    }
+}
